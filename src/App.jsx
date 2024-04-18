90b407a23b9c12bd2aa6ce2062cd3024d548ee9d
rakshith_s <rakshithsantosh@gmail.com>
2024-04-18 23:17:44 +0530

    hero component complete design

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./app.scss"
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return <div>
-    <section id='Homepage'><Navbar/></section>
+    <section id='Homepage'><Navbar/><Hero/></section>
     <section id='Services'>Parallax</section>
     <section>Services</section>
     <section id="Portfolio">Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section id="Contact">Contact</section>
+    <section id="Contact-">Contact</section>
   </div>;
 };
 
